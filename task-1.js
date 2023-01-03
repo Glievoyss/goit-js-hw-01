1c726125f6b258d27265c679ad4f166377e3bd26
1 <a1@MacBook-Pro-1.local>
2023-01-03 21:31:01 +0200

    vspomogalka notarius

--- task-1.js
+++ task-1.js
@@ -1,11 +1,47 @@
-'use strict';
+//srick
 
-const name = 'Генератор защитного поля';
-let price = 1000;
+const arrReestr = [8, 3, 3, 3, 0, 0];
 
-console.log(`Выбран ${name}, цена за штуку ${price} кредитов.`);
+const parsProebanyh = (arrReestr) => {
+  let listProebanyh = [];
+  const min = Math.min(...arrReestr);
+  const max = Math.max(...arrReestr);
 
-price = 2000;
+  for (let i = min; i <= max; i++) {
+    if (!arrReestr.includes(i)) {
+      listProebanyh.push(i);
+    }
+  }
 
-console.log(`Выбран ${name}, цена за штуку ${price} кредитов.`);
+  return { min: min, max: max, proebanye: listProebanyh };
+};
 
+const listPr = parsProebanyh(arrReestr);
+
+console.log(
+  "Минимальное:",
+  `${listPr.min},`,
+  "Максимальное:",
+  `${listPr.max},`,
+  "Список проебаных:",
+  listPr.proebanye
+);
+let count = {};
+
+for (let elem of arrReestr) {
+  if (count[elem] === undefined) {
+    count[elem] = 1;
+  } else {
+    count[elem]++;
+  }
+}
+
+const keyObj = Object.keys(count);
+
+keyObj.forEach((item) => {
+  if (count[item] === 1) {
+    delete count[item];
+  }
+});
+
+console.log("Дубликаты", count);
